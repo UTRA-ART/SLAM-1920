95811e73d76ac5bd4a500909685359786bd68a4e
tcheung99 <38997117+tcheung99@users.noreply.github.com>
2020-03-28 20:34:39 -0600

    Changed print to rospy.loginfo()

--- slam-ws/src/load_waypoints/scripts/load_waypoints_server.py
+++ slam-ws/src/load_waypoints/scripts/load_waypoints_server.py
@@ -12,18 +12,18 @@ def populate_waypoint_table():
         try:
             data = json.load(f)
         except ValueError, e:
-            print("Invalid JSON")
+            rospy.loginfo("Invalid JSON")
             sys.exit(1)
 
     # Parse through dictionary and create list of lists holding all waypoints
     for waypoint in data["waypoints"]:
         all_waypoints.append([waypoint['coordinate 1'], waypoint['coordinate 2']])
 
-    print("Loaded waypoints:", all_waypoints)
+    rospy.loginfo("Loaded waypoints:", all_waypoints)
     return 
 
 def handle_waypoint_request(waypoint_request):
-    print ("Returning request for waypoint #%s "%(waypoint_request.waypoint_number))
+    rospy.loginfo("Returning request for waypoint #%s "%(waypoint_request.waypoint_number))
     return WaypointRequestResponse(all_waypoints[waypoint_request.waypoint_number])
 
 def load_waypoint_server():
@@ -31,7 +31,7 @@ def load_waypoint_server():
     rospy.init_node('load_waypoint_server')
     populate_waypoint_table()
     s = rospy.Service('load_waypoint', WaypointRequest, handle_waypoint_request)
-    print("Ready to load waypoints.")
+    rospy.loginfo("Ready to load waypoints.")
     rospy.spin() # Keeps code from exiting until the service is shutdown
 
 if __name__ == "__main__":
